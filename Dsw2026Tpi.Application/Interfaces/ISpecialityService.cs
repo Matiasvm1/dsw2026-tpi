e3ebf52444dd1b9294b7743869e52c56c92ededa
Nicolas Khouri <Jose.Khouri@alu.frt.utn.edu.ar>
2026-07-29 22:49:57 -0300

    feat(specialities): entidad actualizada, DTO e interfaz creados

---
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Dsw2026Tpi.Application.Dtos;
+using System.Threading.Tasks;
+using Dsw2026Tpi.CrossCutting.Models;
+using Dsw2026Tpi.Domain.Entities;
+
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface ISpecialityService
+{
+    Task<Pagination<SpecialityModel.Response>> GetAll(PaginationQuery pagination, string? name = null);
+    Task<SpecialityModel.Response> Create(SpecialityModel.Request request);
+    Task<SpecialityModel.Response> Update(Guid id, SpecialityModel.Request request);
+    Task Delete(Guid id);
+}
